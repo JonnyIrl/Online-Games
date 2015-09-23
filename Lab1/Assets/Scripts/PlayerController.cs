9423570b041c77fd84aa6829c30435367be34871
JonnyIrl <swtorjonny@hotmail.com>
2015-09-23 12:16:28 +0100

    Lab 1
    Unity simple game collecting coins score updating

---
+++ Lab1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text ScoreText;
+
+    private Rigidbody rb;
+
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        ScoreText.text = "Score = " + count.ToString();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            ScoreText.text = "Score = " + count.ToString();
+        }
+    }
+}
